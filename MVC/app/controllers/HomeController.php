9106b3df23bd74adfc81d22b525f8f9bbfc8788e
alisyahpu3 <11alisyahpu3@gmail.com>
2024-08-16 14:56:24 +0700

    first commit

---
+++ MVC/app/controllers/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+class HomeController{
+    private $userModel;
+    public function __construct(){
+        $this->userModel = new User();
+    }
+    public function index(){
+        $users = $this->userModel->getAllUsers();
+        $view = 'home';
+        require '../app/views/layout.php';
+    }
+}
+?>
---
+++ MVC/app/views/home.php
@@ -0,0 +1,11 @@
+<h1>Welcome to Home Page</h1>
+<h2>User : </h2>
+<ul>
+    <?php if (isset($users) && is_array($users)): ?>
+        <?php foreach ($users as $user): ?>
+            <li><?php echo htmlspecialchars($user['name']); ?></li>
+        <?php endforeach; ?>
+    <?php else: ?>
+        <li>No users available.</li>
+    <?php endif; ?>
+</ul>
