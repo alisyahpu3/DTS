c3cc9c86a1565cf0e8f060b2d963cce084ec006c
alisyahpu3 <11alisyahpu3@gmail.com>
2024-08-23 15:43:41 +0700

    facadequerybuild

---
+++ pwl_pos/resources/views/kategori.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>QUERY BUILDER</title>
+    </head>
+    <body>
+        <h1>DATA USER</h1>
+        <table border ="1" cellpadding ="2" cellspacing ="0">
+            <tr>
+                <th>ID</th>
+                <th>KODE LEVEL</th>
+                <th>nama level</th>
+            </tr>
+            @foreach($data as $d)
+            <tr>
+                <td>{{$d->kategori_id}}</td>
+                <td>{{$d->kategori_kode}}</td>
+                <td>{{$d->kategori_nama}}</td>
+            </tr>
+            @endforeach
+        </table>
+    </body>
+</html>
